04bb2cb60f0abba213883404be1c680098816b97
Rpkprakash <prakashrprakashr127@gmail.com>
2025-05-06 08:21:41 +0530

    commit

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,5 +1,7 @@
+import {Card,Button} from 'react-bootstrap';
 import './Skills.css'
 import SkillsList from './Skills.json'
+import Certificate from './certificate.json'
 export default function Skills(){
     return(<>
     <div id='layout'>
@@ -11,6 +13,14 @@ export default function Skills(){
    </div>
    <div id='certificate'>
     <h1>certificate</h1>
+
+        {Certificate.map((item)=>
+        <Card id='card'>
+        <Card.Img variant="top" src={item['certificate-img']} />
+        <Card.Body>
+          <Card.Title>{item['certificate-name']}</Card.Title>
+        </Card.Body>
+      </Card>)}
    </div>
    </div>
     </>)
